8fa6e724484735491407f5190754306d18f367d1
Austin Byers <austin.byers@airbnb.com>
2017-09-08 18:25:34 -0700

    Mock out pip install during build test

--- tests/lambda_functions/build_test.py
+++ tests/lambda_functions/build_test.py
@@ -1,11 +1,20 @@
 """Test lambda_functions/build.py."""
 import os
+import pip
 import tempfile
+from typing import List, Set
 import unittest
 from unittest import mock
 import zipfile
 
 from lambda_functions import build
+
+
+def _mock_pip_main(args_list: List[str]) -> None:
+    """Mock pip install just creates the target directory."""
+    directory = args_list[-2]
+    package = args_list[-1]
+    os.makedirs(os.path.join(directory, package))
 
 
 @mock.patch.object(build, 'print')
@@ -18,7 +27,8 @@ class BuildTest(unittest.TestCase):
         self.maxDiff = None  # pylint: disable=invalid-name
         self._tempdir = tempfile.gettempdir()
 
-    def _verify_filenames(self, archive_path, expected_filenames, subset=False):
+    def _verify_filenames(self, archive_path: str, expected_filenames: Set[str],
+                          subset: bool = False):
         """Verify the set of filenames in the zip archive matches the expected list."""
         with zipfile.ZipFile(archive_path, 'r') as archive:
             filenames = set(zip_info.filename for zip_info in archive.filelist)
@@ -27,7 +37,7 @@ class BuildTest(unittest.TestCase):
         else:
             self.assertEqual(expected_filenames, filenames)
 
-    def test_build_analyzer(self, mock_print):
+    def test_build_analyzer(self, mock_print: mock.MagicMock):
         """Verify that a valid zipfile is generated for analyzer Lambda function."""
         build._build_analyzer(self._tempdir)
         self._verify_filenames(
@@ -54,7 +64,7 @@ class BuildTest(unittest.TestCase):
         )
         mock_print.assert_called_once()
 
-    def test_build_batcher(self, mock_print):
+    def test_build_batcher(self, mock_print: mock.MagicMock):
         """Verify that a valid zipfile is generated for the batcher Lambda function."""
         build._build_batcher(self._tempdir)
         self._verify_filenames(
@@ -62,7 +72,7 @@ class BuildTest(unittest.TestCase):
         )
         mock_print.assert_called_once()
 
-    def test_build_dispatcher(self, mock_print):
+    def test_build_dispatcher(self, mock_print: mock.MagicMock):
         """Verify that a valid zipfile is generated for the dispatcher Lambda function."""
         build._build_dispatcher(self._tempdir)
         self._verify_filenames(
@@ -70,7 +80,8 @@ class BuildTest(unittest.TestCase):
         )
         mock_print.assert_called_once()
 
-    def test_build_downloader(self, mock_print):
+    @mock.patch.object(pip, 'main', side_effect=_mock_pip_main)
+    def test_build_downloader(self, mock_pip: mock.MagicMock, mock_print: mock.MagicMock):
         """Verify list of bundled files for the downloader."""
         build._build_downloader(self._tempdir)
         self._verify_filenames(
@@ -78,9 +89,12 @@ class BuildTest(unittest.TestCase):
             {'backoff/', 'cbapi/', 'main.py'},
             subset=True
         )
+        mock_pip.assert_called_once()
         mock_print.assert_called_once()
 
-    def test_build_all(self, mock_print):
+    @mock.patch.object(pip, 'main', side_effect=_mock_pip_main)
+    def test_build_all(self, mock_pip: mock.MagicMock, mock_print: mock.MagicMock):
         """Verify that the top-level build function executes without error."""
         build.build(self._tempdir, downloader=True)
+        mock_pip.assert_called_once()
         self.assertEqual(4, mock_print.call_count)
