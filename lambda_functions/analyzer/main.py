e531db87d60ca4ae24a29873ef2811647c325d10
Gooch <stephanie.gooch@kpmg.co.uk>
2018-05-03 17:44:45 +0100

    updated main.py

--- lambda_functions/analyzer/main.py
+++ lambda_functions/analyzer/main.py
@@ -151,11 +151,17 @@ def analyze_lambda_handler(event: Dict[str, Any], lambda_context: Any) -> Dict[s
             continue
 
         if binary.yara_matches:
-            LOGGER.warning('%s matched YARA rules: %s', binary, binary.matched_rule_ids)
-            binary.save_matches_and_alert(
-                lambda_version, os.environ['YARA_MATCHES_DYNAMO_TABLE_NAME'],
-                os.environ['YARA_ALERTS_SNS_TOPIC_ARN'])
-
+                LOGGER.warning('%s matched YARA rules: %s', binary, binary.matched_rule_ids)
+                binary.save_matches_and_alert(
+                    lambda_version, os.environ['YARA_MATCHES_DYNAMO_TABLE_NAME'],
+                    os.environ['YARA_ALERTS_SNS_TOPIC_ARN'])
+        else:
+            LOGGER.info('%s did not match any YARA rules', binary)
+            binary.safe_alert_only(
+                lambda_version, 
+                os.environ['SAFE_SNS_TOPIC_ARN'])
+                
+             
     # Delete all of the SQS receipts (mark them as completed).
     receipts_to_delete = [msg['receipt'] for msg in event.get('messages', [])]
     if receipts_to_delete:
