8b8cc71b8410b85b096135a37b5307b4d55faad2
Gooch <stephanie.gooch@kpmg.co.uk>
2018-05-18 14:37:26 +0100

    changed stament in lambda iam

--- terraform/lambda_iam.tf
+++ terraform/lambda_iam.tf
@@ -53,7 +53,14 @@ data "aws_iam_policy_document" "binaryalert_analyzer_policy" {
     sid       = "PublishAlertsToSNS"
     effect    = "Allow"
     actions   = ["sns:Publish"]
-    resources = ["${aws_sns_topic.yara_match_alerts.arn}", "${aws_sns_topic.safe_alerts.arn}"]
+    resources = ["${aws_sns_topic.yara_match_alerts.arn}"]
+  }
+
+  statement {
+    sid       = "PublishAlertsToSafeSNS"
+    effect    = "Allow"
+    actions   = ["sns:Publish"]
+    resources = ["${aws_sns_topic.safe_alerts.arn}"]
   }
 
   statement {
