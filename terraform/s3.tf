e28b2e7a45a36e364a55d1790294d670327a859c
Austin Byers <austin.byers@airbnb.com>
2018-06-12 17:43:12 -0700

    Use SSE-S3 instead of SSE-KMS for access logs

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -36,8 +36,7 @@ resource "aws_s3_bucket" "binaryalert_log_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.sse_s3.arn}"
-        sse_algorithm     = "aws:kms"
+        sse_algorithm = "AES256"
       }
     }
   }
